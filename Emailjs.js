49800a681c4418e8577acb7180f721603508578e
NikhithaDharavath <119333180+NikhithaDharavath@users.noreply.github.com>
2022-12-07 14:25:09 +0530

    Create Emailjs.js

---
+++ Emailjs.js
@@ -0,0 +1,25 @@
+function sendMail() {//creating a function
+  var params = {
+    //creating objects calling params
+    name: document.getElementById("name").value,
+    //we are getting name or email or message withid ryt so use the same id's you used in html
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_zs8k13b";//connecting emailjs service id
+  const templateID = "template_t8uga7e";//connecting emailjs template 
+
+    emailjs
+      .send(serviceID, templateID, params)
+      .then((res)=>{
+      //res=response
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);//log the responsee
+        alert("Your message sent successfully!!");//final message alret displaying after message is sent via mail
+  })
+    .catch((err)=>console.log(err));//resetting everything to empty again
+
+}
